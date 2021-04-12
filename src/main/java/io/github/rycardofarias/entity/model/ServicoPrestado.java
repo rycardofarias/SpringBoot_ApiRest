9eba3f280c4dc6ffcfa788b5f237a3a9d7b6ce70
Ricardo <ricardofariasxyz@gmail.com>
2021-04-12 01:39:33 -0300

    refactor ServicoPestrado e ServicoPrestadoRepository e adiconar uma coluna data em ServicoPrestado

--- src/main/java/io/github/rycardofarias/entity/model/ServicoPrestado.java
+++ src/main/java/io/github/rycardofarias/entity/model/ServicoPrestado.java
@@ -4,10 +4,11 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Data
-public class Servico {
+public class ServicoPrestado {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +23,7 @@ public class Servico {
 
     @Column
     private BigDecimal valor;
+
+    @Column
+    private LocalDate data;
 }
--- src/main/java/io/github/rycardofarias/entity/repository/ServicoPrestadoRepository.java
+++ src/main/java/io/github/rycardofarias/entity/repository/ServicoPrestadoRepository.java
@@ -3,5 +3,5 @@ package io.github.rycardofarias.entity.repository;
 import io.github.rycardofarias.entity.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ServicoRepository extends JpaRepository<Cliente, Integer>{
+public interface ServicoPrestadoRepository extends JpaRepository<Cliente, Integer>{
 }
