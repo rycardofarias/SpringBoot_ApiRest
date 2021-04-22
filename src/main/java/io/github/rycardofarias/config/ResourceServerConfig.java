09a668cded2d41b3fc692d223014372aa5ecc445
Ricardo <ricardofariasxyz@gmail.com>
2021-04-22 03:49:08 -0300

    Corrigindo o Resource Server

--- src/main/java/io/github/rycardofarias/config/ResourceServerConfig.java
+++ src/main/java/io/github/rycardofarias/config/ResourceServerConfig.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfiguration {
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                     .antMatchers("/api/usuarios").permitAll()
