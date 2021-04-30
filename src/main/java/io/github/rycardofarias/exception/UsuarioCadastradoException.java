b7fcd603f2bafb0980a6a09a88ffb1b8a3a46c16
Ricardo <ricardofariasxyz@gmail.com>
2021-04-29 22:34:49 -0300

    Espaço

--- src/main/java/io/github/rycardofarias/exception/UsuarioCadastradoException.java
+++ src/main/java/io/github/rycardofarias/exception/UsuarioCadastradoException.java
@@ -3,6 +3,6 @@ package io.github.rycardofarias.exception;
 public class UsuarioCadastradoException extends RuntimeException{
 
     public UsuarioCadastradoException(String login){
-        super("Usuario já cadastrado para o login" + login);
+        super("Usuario já cadastrado para o login " + login);
     }
 }
